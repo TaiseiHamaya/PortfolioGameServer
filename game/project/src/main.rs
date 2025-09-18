15089babe1f813558299933db66084b0df6a1f9c
TaiseiHamaya <146906391+TaiseiHamaya@users.noreply.github.com>
2025-09-19 01:18:30 +0900

    ProtocolBuffersの追加を行いました。

--- game/project/src/main.rs
+++ game/project/src/main.rs
@@ -2,6 +2,8 @@ mod game;
 mod entity;
 mod containts;
 
+mod proto;
+
 #[tokio::main]
 async fn main() {
     game::framework::run().await;
---
+++ game/project/src/proto.rs
@@ -0,0 +1,11 @@
+#[path = "proto/types.u.pb.rs"]
+
+mod proto_types;
+
+pub use proto_types::*;
+
+#[path = "proto/math.u.pb.rs"]
+
+mod proto_math;
+
+pub use proto_math::*;
